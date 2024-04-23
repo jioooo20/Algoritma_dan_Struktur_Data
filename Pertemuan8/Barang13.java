d2e71b8cc7959de8951a8113e0533b58e263bd27
Yeon <giojio825@gmail.com>
2024-04-23 17:04:17 +0700

    stack

---
+++ Pertemuan8/Barang13.java
@@ -0,0 +1,135 @@
+package Pertemuan8;
+
+import java.util.Scanner;
+
+public class Barang13 {
+    public int kode;
+    public String nama, kategori;
+
+    public Barang13(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+}
+
+class Gudang13 {
+    Barang13[] tumpukan;
+    int size, top;
+
+    public Gudang13(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang13[size];
+        top = -1;
+    }
+
+    public boolean cekKosong() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cekPenuh() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahBarang(Barang13 brg) {
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambahkan ke gudang");
+        } else {
+            System.out.println("Gagal! Tumpukan barang di gudang sudab penuh");
+        }
+    }
+
+    public Barang13 ambiBarang13() {
+        if (!cekKosong()) {
+            Barang13 delete = tumpukan[top];
+            top--;
+            System.out.println("Barang " + delete.nama + " diambil dari gudang");
+            return delete;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public Barang13 lihatBarangTeratas() {
+        if (!cekKosong()) {
+            Barang13 barangTeratas = tumpukan[top];
+            System.out.println("Barang teratas : " + barangTeratas.nama);
+            return barangTeratas;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public void tampilkanBarang() {
+        if (!cekKosong()) {
+            System.out.println("Rincian tumpukan barang di Gudang : ");
+            // for (int i = top; i >=0; i--) {
+            for (int i = 0; i < top; i++) {
+                System.out.printf("Kode %d: %s (Kategori $s)\n", tumpukan[i].kode, tumpukan[i].nama,
+                        tumpukan[i].kategori);
+            }
+        } else {
+            System.out.println("Tumpukan barang kosong");
+        }
+    }
+
+}
+
+class utama13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Gudang13 gudang = new Gudang13(7);
+
+        while (true) {
+            System.out.println("\nMenu");
+            System.out.println("1. Tambah barang");
+            System.out.println("2. Ambil barang");
+            System.out.println("3. Tampilkan tumpukan barang");
+            System.out.println("4. keluar");
+            System.out.print("Pilih Operasi: ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode barang: ");
+                    int kode = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Masukkan nama barang: ");
+                    String nama = scanner.nextLine();
+                    System.out.print("Masukkan nama kategori: ");
+                    String kategori = scanner.nextLine();
+                    Barang13 barangBaru = new Barang13(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    gudang.ambiBarang13();
+                break;
+                case 3:
+                    gudang.tampilkanBarang();
+                break;
+                case 4:
+                break;
+                default:
+                System.out.println("Pilih angka 1 - 4");
+                    break;
+            }
+        }
+
+
+
+
+    }
+}
